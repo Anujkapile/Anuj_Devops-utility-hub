cd9a4e0b8f80454d1834016a7d70cc14576de488
Anujkapile <kapileanuj15@gmail.com>
2026-03-27 20:06:58 +0530

    Update Terraform config: modified EC2 setup and outputs

--- deployments/Terraform/main.tf
+++ deployments/Terraform/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "ap-south-1"   # Mumbai — change if needed
 }
-
+#===================Security Group: APP SERVER===========================================================================
 resource "aws_security_group" "anuj_sg" {
   name        = "anuj"
   description = "Allow SSH, HTTP, HTTPS, 5000"
@@ -35,13 +35,14 @@ resource "aws_security_group" "anuj_sg" {
     cidr_blocks = ["0.0.0.0/0"]   # Flask app
   }
 
-  egress {
+  ingress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+#=================EC2:App server============================================================
 
 resource "aws_instance" "devops_hub" {
   ami                    = "ami-0a14f53a6fe4dfcd1"  # Ubuntu 22.04 ap-south-1
@@ -56,6 +57,65 @@ resource "aws_instance" "devops_hub" {
 
   tags = {
     Name = "DevOpsUtilityHub"
+}
+
+}
+#===================Security Group: APP SERVER===========================================================================
+
+resource "aws_security_group" "jenkins_sg" {
+  name        = "jenkins_sg"
+  description = "Jenkins + SSH"
+  vpc_id      = "vpc-00ed19a1c09f13a9d"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 50000
+    to_port     = 50000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+# ─── EC2: Jenkins Server ─────────────────────────────────────────────
+
+resource "aws_instance" "jenkins_sg" {
+  ami           = "ami-0a14f53a6fe4dfcd1"
+  instance_type = "t2.micro"
+  key_name      = "devops-key"
+  vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
+
+  root_block_device {
+    volume_size = 20
+    volume_type = "gp3"
+  }
+  tags = { 
+    Name = "JenkinsCICD"  
+    }
 }
 
